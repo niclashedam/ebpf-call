f88d31946f244d96ee3f99fc909315062aceb2d6
Niclas Hedam <nhed@itu.dk>
2021-05-28 16:52:43 +0200

    Updating tests to use MMAP

--- examples/basic.c
+++ examples/basic.c
@@ -1,3 +1,9 @@
 #include "../src/exts.h"
 
-int prog(void *mem, size_t mem_size) { return test(); }
+int prog(void *mem, size_t mem_size) {
+  char *state = mem;
+  state[0] = '1';
+  state[1] = '\0';
+
+  return test();
+}
